b2de07e96b95fc1ddb7bc16eda848a1b735eee70
Forkxel <pavel.halik06@gmail.com>
2025-12-14 23:15:28 +0100

    Created class for questions.

---
+++ WebQuizGame/Classes/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace WebQuizGame.Classes;
+
+public class Question
+{
+    public string Text { get; set; } = string.Empty;
+    public string CorrectAnswer { get; set; } = string.Empty;
+    public List<string> Answers { get; set; } = new List<string>();
+}
