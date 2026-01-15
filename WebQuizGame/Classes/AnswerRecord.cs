943714aa84a1c433357d7de553105e911092aaf5
Forkxel <pavel.halik06@gmail.com>
2026-01-15 22:49:33 +0100

    Added final screen plus reset game.

---
+++ WebQuizGame/Classes/AnswerRecord.cs
@@ -0,0 +1,8 @@
+﻿namespace WebQuizGame.Classes;
+
+public class AnswerRecord
+{
+    public Question Question { get; set; }
+    public bool IsCorrect { get; set; }
+    public int TimeTaken { get; set; }
+}
